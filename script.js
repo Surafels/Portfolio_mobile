fb002dc1e18f48c732622870d316866f124dd2ec
Surafels <ssurafel4@gmail.com>
2023-07-25 21:17:54 +0300

    Fix eslint error

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-let humbergerMenu=document.querySelector('#menu-t');
-let closingMenu= document.querySelector('#cancel');
-let mobileMenuContainer= document.querySelector('#mobile-menu');
-let mobileList= document.querySelector('.mobile-list');
+const humbergerMenu = document.querySelector('#menu-t');
+const closingMenu = document.querySelector('#cancel');
+const mobileMenuContainer = document.querySelector('#mobile-menu');
+const mobileList = document.querySelector('.mobile-list');
 function openmenu() {
-mobileMenuContainer.classList.remove('hidden');}
-humbergerMenu.addEventListener("click", openmenu);
-function closemenu() {
-    mobileMenuContainer.classList.add("hidden");
+  mobileMenuContainer.classList.remove('hidden');
 }
-closingMenu.addEventListener("click",closemenu);
-mobileList.addEventListener("click",closemenu);
+humbergerMenu.addEventListener('click', openmenu);
+function closemenu() {
+  mobileMenuContainer.classList.add('hidden');
+}
+closingMenu.addEventListener('click', closemenu);
+mobileList.addEventListener('click', closemenu);

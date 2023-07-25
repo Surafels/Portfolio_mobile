d9e9fcfda31451289f39af6b1c9a6e941c7ed41d
Surafels <ssurafel4@gmail.com>
2023-07-25 13:06:27 +0300

    Add background for mobile menu

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let closingMenu= document.querySelector('#cancel');
 let mobileMenuContainer= document.querySelector('#mobile-menu');
 let mobileList= document.querySelector('.mobile-list');
 function openmenu() {
-    mobileMenuContainer.classList.remove('hidden');}
+mobileMenuContainer.classList.remove('hidden');}
 humbergerMenu.addEventListener("click", openmenu);
 function closemenu() {
     mobileMenuContainer.classList.add("hidden");

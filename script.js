7000def6491c502a9881694b8436a06725a14eaa
Surafels <ssurafel4@gmail.com>
2023-07-26 15:17:57 +0300

    Add script js  objects for the work section

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function closemenu() {
 }
 closingMenu.addEventListener('click', closemenu);
 mobileList.addEventListener('click', closemenu);
+const project=[
+  {
+    id:'card1',
+    title:'Tonic',
+    projectInfo:{
+      campony:'CANOPY',
+      role:'Back End Dev',
+      year:2015
+    },
+  snapshoot:"asset/Snapshoot Portfolio.png",
+description:'Lorem Ipsum is simply dummy text of the printing and type setting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  skills:['HTML','CSS','JavaScript'] ,
+  }
+]
